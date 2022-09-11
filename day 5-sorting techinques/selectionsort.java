21eec7deeb0701881d8d38e723e74ceeb76f5898
NithyaMohan2001 <112246758+NithyaMohan2001@users.noreply.github.com>
2022-09-11 18:17:27 +0530

    Add files via upload

---
+++ day 5-sorting techinques/selectionsort.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class test {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(a[i]);
+    }
+}
