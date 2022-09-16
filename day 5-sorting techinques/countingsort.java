8fe305af94cca6df71f7a552c337c2a401714de3
NithyaMohan2001 <112246758+NithyaMohan2001@users.noreply.github.com>
2022-09-16 02:35:47 -0700

    Create countingsort.java

--- day 5-sorting techinques/countingsort.java
+++ day 5-sorting techinques/countingsort.java
@@ -1,1 +1,31 @@
+public class Main
+{
+    private final int[] arr;
+    public countingsort(int[] arr){
+        this.arr=arr;
+    }
+   
+    public void sort(){
+        int min=Arrays.stream(arr).min().orElse(0);
+        int max=Arrays.stream(arr).max().orElse(Integer.MAX_VALUE);
+        int countarray[]=new int[max-min+1];
+        for(int value:arr){
+            countarray[value-min]++;
+        }
+        int arrayindex=0;
+        for(int i=0;i<max-min+1;i++){
+            while(countarray[i]>0){
+                arr[arrayindex]=i+min;
+                countarray[i]--;
+                arrayindex++;
+            }
+        }
+    }
+	public static void main(String[] args) {
+		int arr[]={5,2,8,7,-2,2,3,3};
+		System.out.println(Arrays.toString(arr));
+		new countingsort(arr).sort();
+		System.out.println(Arrays.toString(arr));
+	}
+}
 
