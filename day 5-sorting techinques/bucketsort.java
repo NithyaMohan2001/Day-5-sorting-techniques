7d365f0796a41daab5d50e06c07802064660faf8
NithyaMohan2001 <112246758+NithyaMohan2001@users.noreply.github.com>
2022-09-16 17:59:56 -0700

    Update bucketsort.java

--- day 5-sorting techinques/bucketsort.java
+++ day 5-sorting techinques/bucketsort.java
@@ -1,1 +1,16 @@
-
+public 
+class BucketSort<T extends Number & Comparable<T>>{
+  public List<T> sort(List<T> list,BiFunction<T,Integer,Integer> function){
+    int noofbkts=list.size();
+    Map<Integer,List<T>> buckets=new HashMap<>();
+    IntStream.range(0,noofbkts),forEach(i->buckets.put(i,new LinkedList<>());
+    list.forEach(item->buckets.get(function.apply(item,noofbkts)).add(item));
+    buckets.values().forEach(Collections::sort);
+    return buckets.values().Stream().flatMap(Collection::stream).toList();
+    }
+                                        
+            public static void main(String args[]){
+             List<Float> floats=Arrays.asList(0.42F,0.32F,0.63F,0.82F);
+             print(new BucketSort<Float>().sort(floats,(item,size)->(int)(item/size)));
+                                        
+                                        
